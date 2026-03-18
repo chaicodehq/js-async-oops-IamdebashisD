b06039ca64ac52a902d498f2cb191d978a54c03d
IamdebashisD <adebashisdas626@gmail.com>
2026-03-19 03:32:50 +0530

    Implement LassiStand class with menu management and order processing functionality

--- src/02-lassi-stand.js
+++ src/02-lassi-stand.js
@@ -73,15 +73,61 @@
  */
 export function LassiStand(name, city) {
   // Your code here
+  this.name = name
+  this.city = city
+  this.menu = []
+  this.orders = []
+  this._nextOrderId = 1
 }
 
 // Add prototype methods here:
-// LassiStand.prototype.addFlavor = function(flavor, price) { ... }
-// LassiStand.prototype.takeOrder = function(customerName, flavor, quantity) { ... }
-// LassiStand.prototype.completeOrder = function(orderId) { ... }
-// LassiStand.prototype.getRevenue = function() { ... }
-// LassiStand.prototype.getMenu = function() { ... }
+LassiStand.prototype.addFlavor = function(flavor, price) { 
+  if (price <= 0) return -1
+  
+  const isFlavorExist = this.menu.find( item => item.flavor === flavor)
+  if (isFlavorExist) return -1
+  
+  this.menu.push({flavor, price})
+  
+  return this.menu.length
+}
+LassiStand.prototype.takeOrder = function(customerName, flavor, quantity) { 
+  if (quantity <= 0) return -1
+  
+  const isFlavorExist = this.menu.find( item => item.flavor === flavor)
+  if (!isFlavorExist) return -1
+
+  const order = {
+    id: this._nextOrderId++,
+    customer: customerName,
+    flavor,
+    quantity,
+    total: isFlavorExist.price * quantity,
+    status: "pending"
+  }
+  this.orders.push(order)
+
+  return order.id
+}
+LassiStand.prototype.completeOrder = function(orderId) { 
+  const order = this.orders.find(order => order.id === orderId)
+  if (!order || order.status === "completed") return false
+  order.status = "completed"
+  return true
+}
+LassiStand.prototype.getRevenue = function() {
+  const completedOrders = this.orders.filter( order => order.status === "completed")
+  const totals = completedOrders.reduce((sum, order) => (
+    sum + order.total
+  ), 0)
+  return totals
+
+}
+LassiStand.prototype.getMenu = function() {
+  return [...this.menu]
+}
 
 export function isLassiStand(obj) {
   // Your code here
+  return (obj instanceof LassiStand)
 }
